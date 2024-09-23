1dcf7f06bf3c24a89affbdcfc9d400c8b8efd77b
Steven <6100787@spsd.sk.ca>
2024-09-23 11:50:16 -0600

    Update sketch.js

--- p5/Interactive Scene/sketch.js
+++ p5/Interactive Scene/sketch.js
@@ -1,50 +1,50 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene
+// Steven Huang
+// September 23rd, 2024
+// CS30 P3
+// An Interactive program that run continually and produce outputs that is influenced by user input.
 
-
+//4-state var for background
 let currentBack = 0
+//declare easy to access variable for constantly updating colours
 let currentBackColourR = 28
 let currentBackColourG = 37
 let currentBackColourB = 60
+//4-state var for grass colour/keyboard control
 let currentGrass = 0
+//declare easy to access variable for constantly updating colours
 let currentGrassColourR = 38
 let currentGrassColourG = 139
 let currentGrassColourB = 7
 
+
+//setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
 
   loop()
 }
 
 function draw() {
 
+  //default properties
+
+  textAlign(LEFT);
+  textAlign(TOP);
+
   background(currentBackColourR, currentBackColourG, currentBackColourB);
 
   noStroke();
   fill(255, 255, 255);
 
-  //draws stars
+  //draws stars based on canvas size
   let starc = width * height / 10000
   for (let i = 0; i < starc; i++) {
     circle(Math.floor(Math.random() * width) + 1, Math.floor(Math.random() * height) + 1, Math.floor(Math.random() * 5) + 1);
-
-
-
   }
 
-
-
-
-
-
-  fill(177, 196, 216);
   //draw back mountains - light grey
+  fill(177, 196, 216);
   triangle(0, height / 4, 0, height, width, height * 4 / 5);
   triangle(width / 3, height / 4, 0, height * 3 / 4, width, height * 4 / 5);
   triangle(width * 3 / 5, height / 6, 0, height * 3.5 / 4, width, height * 4 / 5);
@@ -68,19 +68,30 @@ function draw() {
   fill(0, 0, 0, 0);
   circle(mouseX, mouseY - 75, 90);
 
+  //stick man body
   line(mouseX, mouseY - 30, mouseX, mouseY + 50);
   line(mouseX - width * height / 12500, mouseY, mouseX + width * height / 12500, mouseY);
   line(mouseX, mouseY + 50, mouseX - 50, mouseY + 100);
   line(mouseX, mouseY + 50, mouseX + 50, mouseY + 100);
 
-  text("Press Middle Mouse Button")
+  //text instructions/artist's mark
+  strokeWeight(1);
+  textFont('Courier New');
+  textSize(width / 40);
+  text("Press Middle Mouse Button to Change Sky Colour", 0, height * 4 / 5);
+  text("Press Space Bar to Change Grass Colour", 0, height * 8 / 9);
+  textAlign(RIGHT);
+  textAlign(BOTTOM);
+  text("By Steven", width * 49 / 50, height * 49 / 50);
 }
 
+//constantly updating window for canvas to look natural and fit to scale
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 
 }
 
+//middle click function for background colour change
 function mousePressed() {
   if (mouseButton === CENTER) {
 
@@ -109,7 +120,7 @@ function mousePressed() {
   }
 }
 
-
+//spacebar function for grass colour change
 function keyPressed() {
   if (keyCode === 32) {
     if (currentGrass === 0) {
