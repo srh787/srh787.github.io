e54f3da7d26f1bfe4f4da51e12f7c87e2c34a178
Steven <6100787@spsd.sk.ca>
2024-10-22 12:16:12 -0600

    Update sketch.js

--- p5/Cars Cars Cars/sketch.js
+++ p5/Cars Cars Cars/sketch.js
@@ -7,12 +7,19 @@
 
 let eastbound = [];
 let westbound = [];
+let trafficLight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   for (i = 0; i < 20; i++) {
-    eastbound.push(new Cars(random(width), random(height / 2 - 15, height / 2 - 185), round(random(1, 15), 0), 0, color(random(255), random(255), random(255)), round(random(1), 0)));
+    let y = random(height / 2 + 15, height / 2 + 170);
+    eastbound.push(new Cars(round(random()), random(width), y, 0, round(random(1, 5), 0), color(random(255), random(255), random(255))));
+  }
+
+  for (i = 0; i < 20; i++) {
+    let y = random(height / 2 - 35, height / 2 - 195);
+    westbound.push(new Cars(round(random()), random(width), y, 1, round(random(-1, -5), 0), color(random(255), random(255), random(255))));
   }
 
 }
@@ -22,13 +29,30 @@ function draw() {
 
   drawRoad();
 
-  for (let Cars of eastbound){
-    Cars.execute();
+  for (let Cars of eastbound) {
+    Cars.action();
+  }
+
+  for (let Cars of westbound) {
+    Cars.action();
+  }
+}
+
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    if (keyIsPressed && keyCode === SHIFT) {
+      let y = random(height / 2 - 35, height / 2 - 195);
+      westbound.push(new Cars(round(random()), random(width), y, 1, round(random(-1, -5), 0), color(random(255), random(255), random(255))));
+    }
+    else {
+      let y = random(height / 2 + 15, height / 2 + 170);
+      eastbound.push(new Cars(round(random()), random(width), y, 0, round(random(1, 5), 0), color(random(255), random(255), random(255))));
+    }
   }
 }
 
 class Cars {
-  constructor(x, y, nx, d, c, t) {
+  constructor(t, x, y, d, nx, c) {
     this.xPos = x;
     this.yPos = y;
     this.speed = nx;
@@ -39,69 +63,74 @@ class Cars {
 
   display() {
     fill(this.color);
+    strokeWeight(0.5);
+    stroke(255);
     if (this.type === 0) {
-      rect(this.x, this.y, 40, 20);
-      rect(this.x + 3, this.y - 1, 8, 1);
-      rect(this.x + 3, this.y + 20, 8, 1);
-      rect(this.x + 29, this.y - 1, 8, 1);
-      rect(this.x + 29, y + 20, 8, 1);
+      rect(this.xPos, this.yPos, 40, 20);
+      rect(this.xPos + 3, this.yPos - 1, 8, 1);
+      rect(this.xPos + 3, this.yPos + 20, 8, 1);
+      rect(this.xPos + 29, this.yPos - 1, 8, 1);
+      rect(this.xPos + 29, this.yPos + 20, 8, 1);
     }
     if (this.type === 1) {
-      rect(this.x, this.y, 30, 25);
-      stroke(2);
-      line(this.x + 20, this.y, this.x + 20, this.y + 20);
+      rect(this.xPos, this.yPos, 30, 25);
+      strokeWeight(2);
+      stroke(0);
+      line(this.xPos + 20, this.yPos, this.xPos + 20, this.yPos + 25);
     }
   }
 
   move() {
-    this.x += this.speed;
-    if (this.x > width) {
-      this.x = -50;
+    this.xPos += this.speed;
+    if (this.xPos > width) {
+      this.xPos = -50;
     }
-    else if (this.x < -50) {
-      this.x = width;
+    else if (this.xPos < -50) {
+      this.xPos = width;
     }
   }
 
   speedUp() {
-    if (this.speed < 15 && this.speed > -15) {
-      if (this.direction === 0) {
-        this.speed += 0.5;
-      }
-      if (this.direction === 1) {
-        this.speed -= 0.5;
-      }
+    if (this.speed < 15 && this.direction === 0) {
+      this.speed += 0.5;
+    }
+    if (this.speed > -15 && this.direction === 1) {
+      this.speed -= 0.5;
     }
   }
 
   speedDown() {
-    if (this.speed < 15 && this.speed > -15) {
-      if (this.direction === 0) {
-        this.speed -= 0.5;
-      }
-      if (this.direction === 1) {
-        this.speed += 0.5;
-      }
+    if (this.speed > 0 && this.direction === 0) {
+      this.speed -= 0.5;
+    }
+    if (this.speed < 0 && this.direction === 1) {
+      this.speed += 0.5;
     }
   }
 
-  updateColor() {
+  changeColor() {
     this.color = color(random(255), random(255), random(255));
   }
 
-  execute() {
+  action() {
     this.move();
     this.display();
 
-    if (random(100) === 0) {
+    if (random(100) < 1) {
       this.speedUp();
     }
-    if (random(100) === 0) {
+    if (random(100) < 1) {
       this.speedDown();
     }
-    if (random(100) === 0) {
-      this.updateColor();
+    if (random(100) < 1) {
+      this.changeColor();
     }
+  }
+}
+
+class TrafficLight {
+  constructor() {
+
   }
 }
 
@@ -112,11 +141,13 @@ function drawRoad() {
   rect(0, height / 2 - 200, width, 400);
 
   stroke(255);
-  strokeWeight(10);
+  strokeWeight(5);
 
   for (let i = 0; i < width; i += 40) {
     line(i, height / 2, i + 20, height / 2);
   }
+}
 
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
