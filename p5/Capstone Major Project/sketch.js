daf9ebb9ce0c9cff8ff779057dd33128aa638b7c
srh787 <6100787@spsd.sk.ca>
2024-12-05 11:00:08 -0600

    background

--- p5/Capstone Major Project/sketch.js
+++ p5/Capstone Major Project/sketch.js
@@ -5,11 +5,24 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let bg_space;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  bg_space = loadImage('assets/space.jpg');
 }
 
 function draw() {
   background(220);
+  image(bg_space, 0, 0, width, height);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+}
+
+class Planet{
+
+
 }
