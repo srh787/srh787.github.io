6af93da2d73d3ed7ba7c6d1cffc09c1973badad0
srh787 <6100787@spsd.sk.ca>
2024-12-10 12:17:03 -0600

    Dec 10

--- p5/Capstone Major Project/sketch.js
+++ p5/Capstone Major Project/sketch.js
@@ -7,14 +7,24 @@
 
 let bg_space;
 
+let earth_tex;
+let moon_tex;
+let mars_tex;
+
+let earth;
+
+function preload() {
+  earth_tex = loadImage('assets/earth.jpg');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   bg_space = loadImage('assets/space.jpg');
 }
 
 function draw() {
   background(220);
-  image(bg_space, 0, 0, width, height);
+  image(bg_space, -width, -height, width*2, height*2);
 }
 
 function windowResized() {
