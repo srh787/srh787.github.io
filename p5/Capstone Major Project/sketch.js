26eacbbe1bb6022947297d8bcee9de29dca96fd9
srh787 <6100787@spsd.sk.ca>
2024-12-12 12:17:02 -0600

    Dec 12

--- p5/Capstone Major Project/sketch.js
+++ p5/Capstone Major Project/sketch.js
@@ -15,16 +15,29 @@ let earth;
 
 function preload() {
   earth_tex = loadImage('assets/earth.jpg');
+  bg_space = loadImage('assets/space.jpg');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  bg_space = loadImage('assets/space.jpg');
+  let canvas = createCanvas(600, 600, WEBGL);
+  // Disable the context menu on the canvas so the camera can use the right mouse button
+  canvas.elt.oncontextmenu = () => false;
+
+  cam = createEasyCam({ distance: 500 });
+
+
+  //earth = new Planet(200, 0, 0, earth_tex);
+  //earth.spawnMoons(4,1);
 }
 
 function draw() {
   background(220);
   image(bg_space, -width, -height, width*2, height*2);
+
+  ambientLight(255, 255, 255);
+  pointLight(255, 255, 255, 0, 0, 0);
+  //earth.show();
+  //earth.orbit();
 }
 
 function windowResized() {
