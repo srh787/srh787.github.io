63c1a8662409a61f1a44c936b22ebd6075494886
srh787 <6100787@spsd.sk.ca>
2024-12-19 12:17:20 -0600

    Dec 19

--- p5/19 Images/sketch.js
+++ p5/19 Images/sketch.js
@@ -5,24 +5,63 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let pilot;
+let pilot;   //p5.Image
 
 function preload(){
-  pilot = loadImage('assets/aviator.png');
+   pilot = loadImage("assets/aviator.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(pilot.width, pilot.height);
+}
+
+function drawCharacter(x,y,avg){
+  fill(255);
+  if(avg > 200) text("0",x,y);
+  else if(avg > 140) text("p",x,y);
+  else if(avg > 90) text("c",x,y);
+  else if(avg > 40) text(".",x,y);
 }
 
 function draw() {
-  image(pilot, 0, 0);
-  loadPixels(); // allows us to access pixels on canvas
+  image(pilot,0,0);
+  loadPixels();  //allows us to access pixels on Canvas
+  background(0);   // . c  # 0
+  
+  for(let x = 0; x < width; x+= 10){
+    for(let y = 0; y < height; y+= 10){
+      let location = (x + y*pilot.width)*4;
+      let avg = avgPixel(location);
+      drawCharacter(x,y,avg);
+    }
+  }
 
+  //Loop through all the pixels:
+  // for(let i = 0; i < pixels.length; i+= 4){
+    //lighten filter
+    // let boost = map(mouseX, 0, width, 0,200);
+    // let r = min(pixels[i] + boost, 255);
+    // let g = min(pixels[i+1] + boost, 255);
+    // let b = min(pixels[i+2] + boost, 255);
+    // setPixelColor(i, r,g,b);
+    
+    //greyscale filter
+    // let a = avgPixel(i);
+    // setPixelColor(i,a,a,a);
+  // }
 
-  updatePixels();
+  //updatePixels();
+}
+
+function avgPixel(pos){
+  //get avg intensity at pos in pixel
+  let sum = pixels[pos] + pixels[pos+1] + pixels[pos+2];
+  return sum/3;
 }
 
 function setPixelColor(pos, r, g, b){
-  pixels[]
+  //set a pixel (starting at pos) to a new RGB value
+  pixels[pos] = r;
+  pixels[pos+1] = g;
+  pixels[pos+2] = b;
 }
